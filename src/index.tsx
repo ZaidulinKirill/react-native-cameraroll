27b99dc8fd3c3385f302d0c4e7c8d2c324103ac7
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-04-03 17:04:42 +0300

    fix names

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { CameraRoll } from './CameraRoll';
 
 export * from './CameraRoll';
 
-export const getAssets = CameraRoll.getAssets;
+export const fetchAssets = CameraRoll.getAssets;
+export const fetchAssetsCount = CameraRoll.getAssetsCount;
 export const editIsFavorite = CameraRoll.editIsFavorite;
 export const deleteAssets = CameraRoll.deleteAssets;
