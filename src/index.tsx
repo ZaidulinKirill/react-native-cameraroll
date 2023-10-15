70507da176406169a704bb316558b477b8455494
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-10-15 18:22:22 +0300

    fix export

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { CameraRoll } from './CameraRoll';
 
 export * from './CameraRoll';
+export * from './types';
 
 export const fetchAssets = CameraRoll.getAssets;
 export const fetchAssetsCount = CameraRoll.getAssetsCount;
